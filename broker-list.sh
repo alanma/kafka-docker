cdb0c3945ba121df9188d96207f873e1e873a511
Ma, Alan <alan.ma@ymail.com>
2015-06-18 10:18:55 -0700

    updated broker/zookeeper listing script; auto-seed broker and zk hosts in start-kafka-shell script;

--- broker-list.sh
+++ broker-list.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+export HOST_IP=`boot2docker ip`
+echo 'HOST_IP='$HOST_IP
+
 CONTAINERS=$(docker ps | grep 9092 | awk '{print $1}')
 BROKERS=$(for CONTAINER in $CONTAINERS; do docker port $CONTAINER 9092 | sed -e "s/0.0.0.0:/$HOST_IP:/g"; done)
-echo $BROKERS | sed -e 's/ /,/g'
+export DOCKER_BROKERS=`echo $BROKERS | sed -e 's/ /,/g'`
+echo 'DOCKER_BROKERS='$DOCKER_BROKERS
+
+CONTAINERS=$(docker ps | grep 2181 | awk '{print $1}')
+ZOOKEEPERS=$(for CONTAINER in $CONTAINERS; do docker port $CONTAINER 2181 | sed -e "s/0.0.0.0:/$HOST_IP:/g"; done)
+export DOCKER_ZOOKEEPERS=`echo $ZOOKEEPERS | sed -e 's/ /,/g'`
+echo 'DOCKER_ZOOKEEPERS='$DOCKER_ZOOKEEPERS
--- start-kafka-shell.sh
+++ start-kafka-shell.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$1 -e ZK=$2 -i -t wurstmeister/kafka:0.8.2.0 /bin/bash
+
+#### When you run a script it gets its own shell and its own environment, which disappears again as soon as the script is finished. To keep the environment variables around, source the script into your shell/script:
+source broker-list.sh
+
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$HOST_IP -e ZK_PORT_2181_TCP_ADDR=$DOCKER_ZOOKEEPERS -i -t kafkadocker_kafka:latest /bin/bash
